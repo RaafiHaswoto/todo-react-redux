59b00a3de0f85c2696afeb7f3e9af0f11a2f56d2
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-22 20:39:19 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import App from './components/App'
-
-import "semantic-ui-css/semantic.min.css";
-
+import App from './App'
 
 
 render(<App />, document.getElementById("root"));
