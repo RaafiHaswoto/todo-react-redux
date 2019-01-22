7a982fa52111e122242784b830e65a59d59eba8b
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-22 22:04:09 +0700

    bug update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ class App extends Component {
     return (
       <div className="App-Header">
         <h2>Welcome To TODO List Using React-Redux</h2>
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              {this.props.todos.map(todo => (
+                <li key={todo.id}>
+                  <input type="checkbox" defaultChecked={todo.isComplete}/>{todo.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
