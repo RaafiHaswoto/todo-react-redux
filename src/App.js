29dac258400f5ea51f0f4c37d04dc69f24df4e43
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-22 20:23:56 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
-    render() { 
-        return ( 
-
-         );
-    }
+  render() {
+    return (
+      <div className="App-Header">
+        <h2>Welcome To TODO List Using React-Redux</h2>
+      </div>
+    );
+  }
 }
- 
+
 export default App;
