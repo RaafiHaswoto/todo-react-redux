f5ee4903d08952991b183a18a51c5dd0f5dc68e5
RaafinHaswoto <raafi.haswoto@gmail.com>
2019-01-22 22:03:59 +0700

    added todos object

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import App from './App'
 
-
-render(<App />, document.getElementById("root"));
+const state = {
+    todos: [
+        {id: 1, name: 'Main Bola', isComplete: true}, 
+        {id: 2, name: 'Main PES', isComplete: true},
+        {id: 3, name: 'Main PUBG', isComplete: true}
+    ]
+}
+render(<App todos={state.todos}/>, document.getElementById("root"));
